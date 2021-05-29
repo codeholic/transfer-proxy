4a88f03e9cda31718f11a01151297096a7891cdc
Ivan Fomichev <ifomichev@gmail.com>
2021-05-29 11:28:55 +0200

    add transfer proxy

--- programs/transfer-proxy/src/lib.rs
+++ programs/transfer-proxy/src/lib.rs
@@ -1,9 +1,33 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program;
 
 #[program]
 pub mod transfer_proxy {
     use super::*;
+
     pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn transfer(ctx: Context<Transfer>, lamports: u64, nonce: u8) -> ProgramResult {
+        let transfer_proxy_account = &mut ctx.accounts.transfer_proxy_account;
+        let from_account = &ctx.accounts.from_pubkey;
+        let to_account = &ctx.accounts.to_pubkey;
+
+        transfer_proxy_account.transfer_count += 1;
+
+        let ix = solana_program::system_instruction::transfer(
+            from_account.key,
+            to_account.key,
+            lamports,
+        );
+
+        solana_program::program::invoke_signed(
+            &ix,
+            &[from_account.clone(), to_account.clone()],
+            &[&[b"transfer-proxy", &[nonce]]],
+        )?;
+
         Ok(())
     }
 }
@@ -13,6 +37,16 @@ pub struct Initialize<'info> {
     #[account(init)]
     transfer_proxy_account: ProgramAccount<'info, TransferProxyAccount>,
     rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct Transfer<'info> {
+    #[account(mut)]
+    transfer_proxy_account: ProgramAccount<'info, TransferProxyAccount>,
+    from_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    to_pubkey: AccountInfo<'info>,
+    system_program: AccountInfo<'info>,
 }
 
 #[account]
--- tests/transfer-proxy.js
+++ tests/transfer-proxy.js
@@ -5,6 +5,8 @@ const assert = require('assert');
 const provider = anchor.Provider.env();
 anchor.setProvider(provider);
 
+const { connection } = provider;
+
 const program = anchor.workspace.TransferProxy;
 
 const initializeTx = async () => {
@@ -24,12 +26,56 @@ const initializeTx = async () => {
   return { transferProxyAccount, tx };
 };
 
-describe('transfer-proxy', () => {
-  it('Is initialized!', async () => {
+const airdrop = async (publicKey, lamports) => (
+  await connection.confirmTransaction(
+    await connection.requestAirdrop(publicKey, lamports),
+    'confirmed',
+  )
+);
+
+describe('TransferProxy', () => {
+  it('initialize', async () => {
     const { transferProxyAccount } = await initializeTx();
 
     const transferProxyAccountData = await program.account.transferProxyAccount(transferProxyAccount.publicKey);
-
     assert(transferProxyAccountData.transferCount.eq(new anchor.BN(0)));
+  });
+
+  it('transfer', async () => {
+    const { transferProxyAccount } = await initializeTx();
+
+    let fromAccount = provider.wallet;
+
+    const transferAmount = 10 * anchor.web3.LAMPORTS_PER_SOL;
+    await airdrop(fromAccount.publicKey, transferAmount);
+
+    const toAccount = anchor.web3.Keypair.generate();
+
+    const [_pdaAccount, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      ['transfer-proxy'],
+      program.programId,
+    );
+
+    await connection.confirmTransaction(
+      await program.rpc.transfer(
+        new anchor.BN(transferAmount),
+        nonce,
+        {
+          accounts: {
+            transferProxyAccount: transferProxyAccount.publicKey,
+            fromPubkey: fromAccount.publicKey,
+            toPubkey: toAccount.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+          },
+        },
+      ),
+      'confirmed',
+    );
+
+    const balance = await connection.getBalance(toAccount.publicKey);
+    assert(balance == transferAmount);
+
+    const transferProxyAccountData = await program.account.transferProxyAccount(transferProxyAccount.publicKey);
+    assert(transferProxyAccountData.transferCount.eq(new anchor.BN(1)));
   });
 });
